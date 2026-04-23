7e350023b4d129b8b3a2a35100cc43caa9c25bb3
junichitashiro <junichitashiro926@gmail.com>
2026-04-23 19:24:13 +0900

    不要プロジェクトの整理

--- Frameworks/Django/samples/django_template/sampleproject/sampleapp/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from sampleapp.models import Sample
-
-admin.site.register(Sample)
--- Frameworks/Django/samples/django_template/sampleproject/sampleapp/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from sampleapp.models import Sample
-
-
-class SampleForm(forms.ModelForm):
-    class Meta:
-        model = Sample
-        fields = ('title', 'text')
